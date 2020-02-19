f58a45e44b3df60bb669e25b2e79965f53779139
Rawa08 <rawa_aref@hotmail.com>
2020-02-19 15:10:08 +0000

    created logout functionality

--- django_auth/urls.py
+++ django_auth/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-from konton.views import index
+from konton.views import index, logout
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', index, name='index')
+    url(r'^$', index, name='index'),
+    url(r'^konton/logout/$', logout, name='logout')
 ]
