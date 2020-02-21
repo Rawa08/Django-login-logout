af208b823ca12271448d3f97cb9a43878c5cae0e
Rawa08 <rawa_aref@hotmail.com>
2020-02-21 13:34:12 +0000

    changing debug mode to env var

--- django_auth/settings.py
+++ django_auth/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get('D_S_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = os.environ.get('debug')
 
 ALLOWED_HOSTS = ['django-authentication-rawa.herokuapp.com']
 
